3b19693796c2c6932fbd7c73d3a7733363bc12fb
Thiago Xavier <thiago.xavieraguilar1@gmail.com>
2023-02-24 23:41:25 -0300

    function_openai

--- run.py
+++ run.py
@@ -9,7 +9,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #OpenAI API key
-openai_key = os.environ.get("OPENAI")
+openai.api_key  = os.environ.get("OPENAI")
+#token discord
 token = os.environ.get("DISCORD")
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
@@ -21,39 +22,51 @@ async def on_ready():
 
 @bot.event
 async def on_message(message: any):
+    #ignore messages from the bot itself
     if message.author == bot.user:
         return
+    #check if the message contains a bad word
     if "bad word" in message.content:
         await message.channel.send(f" No bad words {message.author}")
         #deleting bad words
         await message.delete()
+    #check if the message contains a '!' to start the bot
+    elif not "!" in message.content:
+        initial_msg = 'Hello, all comands: !crypto, !private, !chatgpt'
+        await message.channel.send(initial_msg)
     await bot.process_commands(message)
 
 @bot.command(name="start")
 async def send_hello(ctx):
-    response = 'Hello'
+    response = 'Hello, all comands: !crypto, !private, !chatgpt'
     await ctx.send(response)
 
 @bot.command(name="crypto")
-async def send_price_crypto(ctx, crypto: str, base: str):
+async def send_price_crypto(ctx, crypto=None, base=None):
+    """send message with price of crypto"""
     try:
+        #send a GET request to the Binance API with the requested crypto and base symbols
         response = requests.get(f"https://api.binance.com/api/v3/ticker/price?symbol={crypto.upper()}{base.upper()}")
         data = response.json()  
+        #extract the current price of the requested crypto and base pair
         price = data.get("price")
         if price:
+            #create an embed message to display the price
             price = float(price)
             embed = discord.Embed(
             title=f"Price of {crypto.upper()}/{base.upper()}", 
             description=f"The current price of {crypto.upper()}/{base.upper()} is **${price:.2f}**.", 
-            color=0x00ff00
-            )
-
+            color=0x00ff00)
+            #send the embed message to the channel where the command was used
             await ctx.send(embed=embed)
-        
+
         else:
-            await ctx.send('Invalid pair')
+            #if the requested pair is invalid, send an error message with an example of a valid pair
+            await ctx.send('Invalid pair, say !crypto and write the pair of crypto, example: !crypto BTC USDT')
     except:
-        await ctx.send('Error')
+        #if there is an error, send a error message
+        await ctx.send('Error,  say !crypto and write the pair of crypto, example: !crypto BTC USDT')
+
 
 @bot.command(name="private")
 async def message_private(ctx):
@@ -62,5 +75,28 @@ async def message_private(ctx):
         await ctx.author.send("Private message")
     except discord.errors.forbbiden:
         await ctx.send('Error sending message, enable receive messages from any server')
+
+
+@bot.command(name="chatgpt")
+async def ai_response(ctx,*args):
+    """send message with response for openai"""
+    if len(args) == 0:
+        await ctx.send('say !chatgpt and write you question after that, example: !chatgpt who is the president of usa? ')
+    else:
+        try:
+            msg = " ".join(args)
+            response = openai.Completion.create(
+                engine="text-davinci-002",
+                prompt=msg,
+                max_tokens=1024,
+                n=1,
+                stop=None,
+                temperature=0.8,
+                )
+            response_text = response["choices"][0]["text"].strip()
+            await ctx.send(response_text)
+        except:
+            await ctx.send('say !chatgpt and write you question after that, example: !chatgpt who is the president of usa? ')
+    
 
 bot.run(token)
